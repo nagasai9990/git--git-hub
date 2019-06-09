af4fb9014e85c613c98846e3e58568d93667b0aa
nagasai9990 <nagasai9990@gmail.com>
2019-06-10 01:52:07 +0530

    java code

---
+++ JavaExample.java
@@ -0,0 +1,21 @@
+
+package devops;
+
+public class JavaExample {
+
+    public static void main(String[] args) {
+        String str = "This is my Project";
+        String reversed = reverseString(str);
+        System.out.println("The reversed string is: " + reversed);
+    }
+
+    public static String reverseString(String str)
+    {
+        if (str.isEmpty())
+            return str;
+        //Calling Function Recursively
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
+}
+
+
